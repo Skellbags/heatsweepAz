59f5ee4333fb80539d057552772890e423fa344f
root <ryan@iatek.com>
2023-04-24 06:44:51 -0700

    test2

--- backend/heatsweep_backend/settings.py
+++ backend/heatsweep_backend/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
+        'HOST': 'heatsweep-postgres.postgres.database.azure.com',
         'PORT': 5432,
     }
 }
